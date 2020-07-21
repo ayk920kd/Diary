37d0be9dbd9ff1602be7d39298b577978ff4acff
Ayuka-Okada <a.snow.rsc2@gmail.com>
2020-07-21 19:35:30 +0900

    write down Create function

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Diary;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateDiary;
 
 class DiaryController extends Controller
 {
@@ -17,5 +18,22 @@ class DiaryController extends Controller
         return view('diaries.index',['diaries' => $diaries]);
 
         dd($diaries);  //var_dump()とdie()を合わせたメソッド。変数の確認 + 処理のストップ
+    }
+
+    public function create()
+    {
+        // views/diaries/create.blade.phpを表示する
+        return view('diaries.create');
+    }
+
+    public function store(CreateDiary $request)
+    {
+        $diary = new Diary(); //Diaryモデルをインスタンス化
+
+        $diary->title = $request->title; //画面で入力されたタイトルを代入
+        $diary->body = $request->body; //画面で入力された本文を代入
+        $diary->save(); //DBに保存
+
+        return redirect()->route('diary.index'); //一覧ページにリダイレクト
     }
 }
